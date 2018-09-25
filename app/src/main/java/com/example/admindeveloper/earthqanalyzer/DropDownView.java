f50a0c5539de916b4068a73705b4a00e89e8bdac
Clark Jay <clarkjaycortezano@gmail.com>
2018-09-25 16:33:10 +0800

    Earthquake Analyzer

--- app/src/main/java/com/example/admindeveloper/earthqanalyzer/DropDownView.java
+++ app/src/main/java/com/example/admindeveloper/earthqanalyzer/DropDownView.java
@@ -1,10 +1,9 @@
 package com.example.admindeveloper.earthqanalyzer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,7 +13,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class DropDownView extends AppCompatActivity
+
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,6 @@ public class DropDownView extends AppCompatActivity
         setContentView(R.layout.activity_drop_down_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +33,8 @@ public class DropDownView extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //displaySelectedScreen(R.id.nav_orientation);
     }
 
     @Override
@@ -74,28 +69,40 @@ public class DropDownView extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    private void displaySelectedScreen(int itemId) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        //creating fragment object
+        Fragment fragment = null;
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        //initializing the fragment object which is selected
+        /*switch (itemId) {
+            case R.id.nav_orientation:
+                fragment = new CompassPageView();
+                break;
+            case R.id.nav_loaddata:
+                fragment = new LoadDataView();
+                break;
+            case R.id.nav_realtime:
+                fragment = new RealTimeView();
+                break;
+        }*/
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_main, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        displaySelectedScreen(item.getItemId());
         return true;
     }
+
 }
